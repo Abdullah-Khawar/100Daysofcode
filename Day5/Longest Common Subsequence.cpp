e67eecf9fd7f6a4f9b594f1f437bdbe56d0fc129
Abdullah-Khawar <120805469+Abdullah-Khawar@users.noreply.github.com>
2024-01-27 20:16:55 +0500

    Longest Common Subsequence.cpp

--- Day5/Longest Common Subsequence.cpp
+++ Day5/Longest Common Subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int dp[1001][1001];
 
-    int solve(string& s1, string& s2, int i, int j) {
+    int solve(int i, int j, string& s1, string& s2) {
         // Base Case: If either of the strings reaches the end, return 0
         if (i >= s1.length() || j >= s2.length())
             return 0;
@@ -13,17 +13,17 @@ public:
 
         // If the characters at the current positions are the same
         if (s1[i] == s2[j]) {
-            return dp[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
+            return dp[i][j] = 1 + solve(i + 1, j + 1, s1, s2);
         }
 
         // If the characters are different, try both possibilities and choose the maximum
-        return dp[i][j] = max(solve(s1, s2, i, j + 1), solve(s1, s2, i + 1, j));
+        return dp[i][j] = max(solve(i, j + 1, s1, s2), solve(i + 1, j, s1, s2));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
         // Initialize the memoization array with -1
         memset(dp, -1, sizeof(dp));
     
-        return solve(text1, text2, 0, 0);
+        return solve(0, 0, text1, text2);
     }
 };
