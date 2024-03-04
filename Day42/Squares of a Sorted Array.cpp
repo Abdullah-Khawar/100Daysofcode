3d1e67729a3e286d75e302ff852ad6fb6256839f
Abdullah-Khawar <120805469+Abdullah-Khawar@users.noreply.github.com>
2024-03-04 14:07:22 +0500

    Squares of a Sorted Array.cpp

---
+++ Day42/Squares of a Sorted Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        for(auto &it: nums){
+            it = it*it;
+        }
+      
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+};
